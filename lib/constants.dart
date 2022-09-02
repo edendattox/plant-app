a210917507b12235a6fedb3c015f9271f89a6629
eden <edendattox@gmail.com>
2022-09-02 10:46:19 +0530

    proj initialised

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart";
+
+// Colors that we ues in our app
+const kPrimaryColor = Color(0xff0c9869);
+const kTextColor = Color(0xff3c4046);
+const kBackgroundColor = Color(0xfff9f8fd);
+
+const double kDefaultPadding = 20.0;
